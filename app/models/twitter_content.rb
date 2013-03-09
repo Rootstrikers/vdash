b9f05a2af31f5bf84a92aa9db312e344b3f9dc8f
Jon Evans <jle@foraker.com>
2013-03-09 16:10:58 -0700

    Validate that a tweet's body is <= 140 characters

--- app/models/twitter_content.rb
+++ app/models/twitter_content.rb
@@ -1,3 +1,5 @@
 class TwitterContent < ActiveRecord::Base
   include ContentBase
+
+  validates :body, length: { maximum: 140 }
 end
--- spec/models/twitter_content_spec.rb
+++ spec/models/twitter_content_spec.rb
@@ -2,4 +2,6 @@ require 'spec_helper'
 
 describe TwitterContent do
   it_behaves_like 'content'
+
+  it { should ensure_length_of(:body).is_at_most(140) }
 end
