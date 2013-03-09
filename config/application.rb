71d1431c4dfbb1404dee36f4eac3bec6874fc539
Jon Evans <jle@foraker.com>
2013-03-09 16:06:45 -0700

    Configure application to use rspec, factory girl

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,10 @@ module Vdash
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.generators do |g|
+      g.test_framework :rspec, :fixture => true, :views => false
+      g.fixture_replacement :factory_girl, :dir => "spec/factories"
+    end
   end
 end
