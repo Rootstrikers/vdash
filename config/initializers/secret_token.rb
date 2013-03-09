2d31ad35e404cfa8da40f749b1655bb39c9e11e0
Peter Kinnaird <peter.kinnaird@gmail.com>
2013-03-09 15:37:17 -0500

    Initial Project Creation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vdash::Application.config.secret_token = 'd831456343bb7f6c9b1bc04ce8d4748d9a6378a9dcd7bd7f6a495d3c6edfd6e8f5500d4d4ae4da3bc4bdc3ffc2aaa599403fd71431e7745c6b869c2ef2a0a318'
