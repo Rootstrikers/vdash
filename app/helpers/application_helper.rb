a55297c08771af07b4577b27881c8b2857122fb1
Jon Evans <jle@foraker.com>
2013-03-10 21:49:28 -0600

    Add some basic styling for navigation

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def nav_item(text, url, options = {})
+    content_tag(:li, class: (current_page?(url) ? 'active' : '' )) do
+      link_to text, url, options
+    end
+  end
 end
