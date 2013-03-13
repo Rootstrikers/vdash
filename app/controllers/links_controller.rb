0a134e06666d1d6fcd519aee5459acd4a5e7b9a3
Jon Evans <jle@foraker.com>
2013-03-12 22:42:53 -0600

    Revert "Redirect to links list after submitting one."
    This reverts commit e3a6447a3ead61721e711b5da0e33bb60dca4a22.
    
    I feel pretty strongly that this is a bad idea. After links are ordered
    by some heuristic other than newest-first, it will be hard for a user to
    find their newly submitted link.

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -19,7 +19,7 @@ class LinksController < ApplicationController
   def create
     @link = current_user.links.new(params[:link])
     if @link.save
-      redirect_to '/links', flash: { success: 'Link created.' }
+      redirect_to link_url(@link), flash: { success: 'Link created.' }
     elsif @link.link_with_same_url.present?
       redirect_to link_url(@link.link_with_same_url), flash: { alert: 'Link has already been submitted!' }
     else
