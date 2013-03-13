06123ba21555a784bae40b140ae5d1c774a43d24
Jon Evans <jle@foraker.com>
2013-03-12 22:44:51 -0600

    Update spec due to changes to require_user

--- spec/support/shared_examples/user_requirement.rb
+++ spec/support/shared_examples/user_requirement.rb
@@ -2,9 +2,9 @@ shared_examples "it requires a user" do
   context 'when no user is logged in' do
     before { controller.stub(current_user: nil) }
 
-    it 'redirects to the root_url' do
+    it 'redirects to /signin' do
       get :index
-      response.should redirect_to root_url
+      response.should redirect_to '/signin'
     end
   end
 end
