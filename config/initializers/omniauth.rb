23a214de1ae37e0ac7445d69ab7c35b69d17e51e
Jon Evans <jle@foraker.com>
2013-03-27 22:51:13 -0600

    Add ability to sign in with Facebook

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,4 +2,5 @@ OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, TWITTER_CONFIG['app_key'], TWITTER_CONFIG['secret']
+  provider :facebook, FACEBOOK_CONFIG['app_key'], FACEBOOK_CONFIG['secret']
 end
