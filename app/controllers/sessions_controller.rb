2b782e7aec5abab51c10e309e5deacb43fc45124
bradley <bradley.j.griffith@gmail.com>
2013-04-17 22:24:23 -0500

    fixing up alert messages

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
     user = User.from_omniauth(env["omniauth.auth"])
     session[:user_id] = user.id
     next_url = session.delete(:after_signin_url) || root_url
-    redirect_to (next_url), notice: "Hey there, #{user.name}!"
+    redirect_to (next_url), notice: "Hey there, <a href='#{url_for(current_user)}'>#{user.name}</a>! Let's get to work.".html_safe
   end
 
   def destroy
