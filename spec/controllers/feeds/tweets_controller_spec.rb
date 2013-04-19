c5383269c3ed2dd80361c7f0d08399ffe1f8d0d0
Jon Evans <jle@foraker.com>
2013-04-18 23:15:33 -0600

    Whitespace

--- spec/controllers/feeds/tweets_controller_spec.rb
+++ spec/controllers/feeds/tweets_controller_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 module Feeds
   describe TweetsController do
     describe 'a GET to :index' do
-      let(:tweets) { [FactoryGirl.create(:posted_twitter_content), FactoryGirl.create(:posted_twitter_content)]}
+      let(:tweets) { [FactoryGirl.create(:posted_twitter_content), FactoryGirl.create(:posted_twitter_content)] }
       let(:action) { -> { get :index, format: :rss } }
 
       it 'assigns to @tweets, with most recently published first' do
