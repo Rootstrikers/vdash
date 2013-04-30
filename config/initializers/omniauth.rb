9e761889604b0e5b6af1f2a0e9530a9c0388385d
Peter Kinnaird <peter.kinnaird@gmail.com>
2013-04-30 00:06:41 -0400

    Modified Facebook production oauth api keys to use new/official RS ones.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,6 +2,6 @@ OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, ENV['TWITTER_API_KEY'], ENV['TWITTER_API_SECRET'], client_options: { authorize_path: '/oauth/authenticate' }
-  provider :facebook, FACEBOOK_CONFIG['app_key'], FACEBOOK_CONFIG['secret']
+  provider :facebook, ENV['FACEBOOK_API_KEY'], ENV['FACEBOOK_API_SECRET']
   provider :google_oauth2, GOOGLE_CONFIG['app_key'], GOOGLE_CONFIG['secret'], { access_type: 'online', approval_prompt: '' }
 end
