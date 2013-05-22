cd0180d3cce0829a13797a1b0aa9436cfbf7961c
Jon Evans <jle@foraker.com>
2013-05-21 22:36:00 -0600

    Declare dimensions for header images
    This way, the text doesn't jump around since the text loads before the
    svg as the space is reserved for the image.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   def nav_item(text, image, url, options = {})
     content_tag(:li, class: (current_page?(url) ? 'active' : '' )) do
-      icon = image ? "<img src='#{image}' type='image/svg+xml'>" : ''
+      icon = image ? "<img src='#{image}' type='image/svg+xml' width='24' height='24'>" : ''
       link_to raw("#{icon}<h4>#{text}</h4>"), url, options
     end
   end
