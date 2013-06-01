0b6869cb6cba3e60e2ecba0eb90bdc301e650b85
Jon Evans <jle@foraker.com>
2013-05-31 23:10:52 -0600

    Make cookie secret token actually a secret

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Vdash::Application.config.secret_token = 'd831456343bb7f6c9b1bc04ce8d4748d9a6378a9dcd7bd7f6a495d3c6edfd6e8f5500d4d4ae4da3bc4bdc3ffc2aaa599403fd71431e7745c6b869c2ef2a0a318'
+Vdash::Application.config.secret_token = ENV['RS_SECRET_KEY']
